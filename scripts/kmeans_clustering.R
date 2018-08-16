f79931f40cb6cd5e251f0e2139355a1f1898ae98
Rivera <J.C.RIVERA@cgiar.org>
2018-08-16 11:06:23 -0500

    jcr

--- scripts/kmeans_clustering.R
+++ scripts/kmeans_clustering.R
@@ -18,3 +18,17 @@ km_res
 #Calcular la media por cada cluster
 
 aggregate(USArrests, by = list(cluster=km_res$cluster), mean)
+
+#adicionar al cluster original 
+
+dd <- cbind(USArrests, cluster= km_res$cluster)
+km_res$centers
+km_res$size
+
+jpeg('cluster_diagrama.jpg')
+fviz_cluster(km_res, data =df, palette = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"),
+             ellipse.type = "euclid",
+             star.plot = TRUE,
+             repel = TRUE,
+             ggtheme = theme_minimal())
+dev.off()
