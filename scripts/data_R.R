9556a9b91a71d4a447922f83487db0cae60b6fff
Rivera <J.C.RIVERA@cgiar.org>
2018-08-24 14:43:47 -0500

    jcr

---
+++ scripts/data_R.R
@@ -0,0 +1,42 @@
+#R as calculadora
+
+#Operaciones. R as calculadora
+library(MASS)
+
+
+2*(-3.2)
+2(-3,2)
+5^2
+5**2
+sin(2*pi/3)
+log(1)
+c(1,2,3,4,5)
+c(1,2,3,4,5)*2
+
+#Asignacion de variables 
+#Continuacion de la consola
+2*
+3
+
+x <- sin(2*pi/3)
+sin(2*pi/3) -> x
+y <- .Last.value
+rm(y)
+remove(y)
+
+#asignacion de variables con caracteres
+value <- "Hello world"
+#mayusculas y minisculas 
+Value <- "Hello world"
+
+#Espacios
+value <- "Hello  world"
+
+
+#Main function 
+attach(Cars93)
+search()
+names(Cars93)
+find("Cars93")
+
+
