06ea0c855b5075e2757581e291afcf31e4d2139e
Rivera <J.C.RIVERA@cgiar.org>
2018-08-24 15:52:42 -0500

    jcr

--- scripts/data_R.R
+++ scripts/data_R.R
@@ -40,3 +40,26 @@ names(Cars93)
 find("Cars93")
 
 
+#Principal functions 
+value <- 12
+string <- "Hello"
+2<4
+cn <- 4 + 4i
+
+#functions
+mode(value)
+mode(string)
+mode(cn)
+names(Cars93)
+
+#vectors
+
+value_num  <- c(1,2,3,5)
+value_char <- c("koala", "kangaroo", "elephant")
+value_logical <- c(F, T, T,T)
+value_logical2 <- c(FALSE, TRUE, TRUE,TRUE)
+
+error_vector <- c(value_num, value_char)
+
+value_seq <- seq(from = 2, to = 10, by =2 )
+value_logical <- c(1,2,3, rep(3,4), seq(from=1, to=6, by=2))
